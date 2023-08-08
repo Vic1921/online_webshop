7e91c9f54790fb757c4638173a5e33637ee52d7a
Victor Binzar <a12022144@unet.univie.ac.at>
2023-08-08 18:14:37 +0300

    Initial commit

---
+++ src/main/java/at/wst/online_webshop/entities/Customer.java
@@ -0,0 +1,36 @@
+package at.wst.online_webshop.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+//@Inheritance(strategy = InheritanceType.JOINED)
+public class Customer {
+
+    @Id
+    @Column(name = "customer_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
