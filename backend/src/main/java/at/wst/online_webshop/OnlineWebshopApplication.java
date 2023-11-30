b4b340e154bcb6eec0520127548fee1c109d46c2
Anh Philipp Vu <a123456@univie.ac.at>
2023-11-30 15:05:22 +0100

    Introducing testing for the CustomerController.

--- backend/src/main/java/at/wst/online_webshop/OnlineWebshopApplication.java
+++ backend/src/main/java/at/wst/online_webshop/OnlineWebshopApplication.java
@@ -2,6 +2,7 @@ package at.wst.online_webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class OnlineWebshopApplication {
--- backend/src/main/java/at/wst/online_webshop/controller/CustomerController.java
+++ backend/src/main/java/at/wst/online_webshop/controller/CustomerController.java
@@ -4,7 +4,8 @@ package at.wst.online_webshop.controller;
 import at.wst.online_webshop.dtos.CustomerDTO;
 import at.wst.online_webshop.dtos.requests.CreatingCustomerRequest;
 import at.wst.online_webshop.services.CustomerService;
-import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,8 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
 
     //SignUp for a customer
     @PostMapping("/register")
@@ -25,16 +28,11 @@ public class CustomerController {
         try {
             CustomerDTO result = customerService.signUp(request);
             return ResponseEntity.ok(result);
-        } catch (IllegalArgumentException e) {
-            // Handle validation errors
-            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             // Handle other exceptions, e.g., unexpected errors
-            return ResponseEntity.status(500).body("Internal Server Error");
+            logger.error("Exception during customer registration", e);
+            return ResponseEntity.badRequest().body(e.getMessage());
+
         }
     }
-
-
-
-
 }
