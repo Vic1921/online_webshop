242061783ead9c19f9fe3d5d7085e5c4b7771e38
Victor Binzar <a12022144@unet.univie.ac.at>
2024-01-15 01:16:58 +0100

    Changed the NoSQL Report to use the appropriate DTO

--- backend/src/main/java/at/wst/online_webshop/nosql/controller/ReportNoSQLController.java
+++ backend/src/main/java/at/wst/online_webshop/nosql/controller/ReportNoSQLController.java
@@ -1,13 +1,8 @@
 package at.wst.online_webshop.nosql.controller;
 
-import at.wst.online_webshop.controller.ReportController;
-import at.wst.online_webshop.dtos.ProductDTO;
-import at.wst.online_webshop.dtos.ReviewDTO;
 import at.wst.online_webshop.nosql.dtos.ProductNoSqlDTO;
+import at.wst.online_webshop.nosql.dtos.ReviewNoSqlDTO;
 import at.wst.online_webshop.nosql.services.ReportNoSQLService;
-import at.wst.online_webshop.services.ReportService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +16,6 @@ import java.util.List;
 public class ReportNoSQLController {
     private final ReportNoSQLService reportNoSQLService;
 
-    private static final Logger logger = LoggerFactory.getLogger(ReportController.class);
-
-
     @Autowired
     public ReportNoSQLController(ReportNoSQLService reportNoSQLService) {
 
@@ -36,7 +28,7 @@ public class ReportNoSQLController {
     }
 
     @GetMapping("/top-reviewers")
-    public List<ReviewDTO> getTopReviewersReport(@RequestParam double price, @RequestParam int limit){
+    public List<ReviewNoSqlDTO> getTopReviewersReport(@RequestParam double price, @RequestParam int limit){
         return reportNoSQLService.generateTopReviewersReport(price, limit);
     }
 
